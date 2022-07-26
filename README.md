3fc7b7141c35ce73381de07011e4fcf68026b9a8
Freewind <nowindlee@gmail.com>
2022-07-27 00:00:51 +0800

    typescript-webpack-near-view-my-nft-demo

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-TypeScript Webpack Near connect MyNearWallet Demo
+TypeScript Webpack Near View My NFT Demo
 =================================
 
-Near官方提供了漂亮的UI，只需要集成，不过还是挺麻烦的
+登录near的testnet后，如何查看自己拥有的nft。
 
-注意：
-1. 由于使用到了node里的一些库，需要在webpack config中进行fallback，还要安装 `npm i buffer process`等，具体参考：
-   1. https://stackoverflow.com/a/68723223/342235
-   2. https://github.com/microsoft/PowerBI-visuals-tools/issues/365#issuecomment-875479827
-2. png图片的处理
-3. 上面两个问题在vite中无法正确处理，所以目前只能在webpack下构建
+TODO: 还没搞定
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,11 +1,11 @@
-import React, {FC, useCallback, useEffect, useState} from 'react';
+import React, {FC, useCallback, useEffect, useMemo, useState} from 'react';
 
-import {setupWalletSelector, WalletSelector} from '@near-wallet-selector/core';
+import {AccountState, setupWalletSelector, WalletSelector} from '@near-wallet-selector/core';
 import myNearWalletIcon from '@near-wallet-selector/my-near-wallet/assets/my-near-wallet-icon.png';
 import {distinctUntilChanged, map} from 'rxjs';
 import {setupModal, WalletSelectorModal} from '@near-wallet-selector/modal-ui';
 import {setupMyNearWallet} from '@near-wallet-selector/my-near-wallet';
-import {providers} from 'near-api-js';
+import {providers, connect as nearConnect} from 'near-api-js';
 import {AccountView} from 'near-api-js/lib/providers/provider';
 
 import '@near-wallet-selector/modal-ui/styles.css';
@@ -16,10 +16,12 @@ export const NEAR_NETWORK = 'testnet';
 export const NEAR_CONTRACT_ID = 'superfungible.testnet';
 
 export const Hello: FC<Props> = ({}) => {
+
     const [selector, setSelector] = useState<WalletSelector>();
     const [modal, setModal] = useState<WalletSelectorModal>();
-    const [accountId, setAccountId] = useState<string>();
+    const [accounts, setAccounts] = useState<AccountState[]>([]);
     const [accountData, setAccountData] = useState<AccountView>();
+    const [accountId, setAccountId] = useState<string>('');
 
     const init = useCallback(async () => {
         const _selector = await setupWalletSelector({
@@ -35,7 +37,7 @@ export const Hello: FC<Props> = ({}) => {
         });
         const state = _selector.store.getState();
 
-        setAccountId(state.accounts.find(it => it.accountId)?.accountId);
+        setAccounts(state.accounts);
 
         setSelector(_selector);
         setModal(_modal);
@@ -61,8 +63,7 @@ export const Hello: FC<Props> = ({}) => {
             )
             .subscribe((nextAccounts) => {
                 console.log('Accounts Update', nextAccounts);
-
-                setAccountId(nextAccounts.find(it => it.active)?.accountId);
+                setAccounts(nextAccounts);
             });
 
         return () => subscription.unsubscribe();
@@ -102,7 +103,7 @@ export const Hello: FC<Props> = ({}) => {
                 const wallet = await selector.wallet();
                 await wallet.signOut();
             }
-            setAccountId(undefined);
+            setAccountId('');
             setAccountData(undefined);
         }
 
@@ -112,11 +113,51 @@ export const Hello: FC<Props> = ({}) => {
         });
     }, [selector]);
 
-    return accountId && accountData ?
-        <div>
-            <h2>Account: </h2>
-            <pre>${JSON.stringify({accountId, accountData}, null, 4)}</pre>
-            <button onClick={logout}>Logout</button>
-        </div>
-        : <button type='button' onClick={connectMyNearWallet}>Connect MyNearWallet</button>
+    useEffect(() => {
+        setAccountId(accounts?.[0]?.accountId ?? '')
+    }, [accounts])
+
+    const nfts = useMemo(async () => {
+        if (accountId) {
+            const near = await nearConnect({
+                nodeUrl: `https://rpc.${NEAR_NETWORK}.near.org`,
+                headers: {},
+                networkId: NEAR_NETWORK
+            });
+            const account = await near.account(accountId);
+            // FIXME will throw error
+            const nfts = await account.viewFunction(accountId, 'nft_tokens_for_owner', {
+                account_id: accountId
+            })
+            console.log("### s", nfts);
+            return nfts;
+        }
+        return undefined;
+    }, [accountId])
+
+    if (accounts.length === 0) {
+        return <button type='button' onClick={connectMyNearWallet}>Connect MyNearWallet</button>;
+    }
+
+    return <>
+        <select value={accountId} onChange={(event) => {
+            console.log('event.target.selectedIndex', event.target.selectedIndex)
+            setAccountId(accounts[event.target.selectedIndex]?.accountId)
+        }}>
+            {accounts.map(it =>
+                <option key={it.accountId} value={it.accountId}>
+                    {it.accountId} ({it.active ? 'active' : 'inactive'})
+                </option>)}
+        </select>
+        <button onClick={logout}>Logout</button>
+        <hr/>
+        <pre>{JSON.stringify({accountId, accounts, accountData, nfts}, null, 4)}</pre>
+    </>
+    // return  accountData ?
+    //     <div>
+    //         <h2>Account: </h2>
+    //         <pre>${JSON.stringify({accounts, accountData}, null, 4)}</pre>
+    //         <button onClick={logout}>Logout</button>
+    //     </div>
+    //     :
 }
